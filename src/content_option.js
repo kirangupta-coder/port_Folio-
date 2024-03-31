e030ab49d3b8b2c61e1b27856e067b1bad35b5c0
kiran gupta <kirangupta4424@gmail.com>
2024-03-31 21:09:15 +0530

    This is portfolio website commit

---
+++ src/content_option.js
@@ -0,0 +1,163 @@
+import Portfoliyo_img from "./assets/images/Portfoliyo_img.avif"
+
+const logotext = "";
+const meta = {
+    title: "KUMARI KIRAN GUPTA ",
+    description: "I’m Kumari Kiran Gupta Full stack devloper,currently working in Kolkata , India",
+};
+
+const introdata = {
+    title: "I’m Kumari Kiran Gupta",
+    description: "Self-driven, quick learner, passionate programmer with a curious mind who enjoys solving a complex and challenging real-world problems..",
+    your_img_url: Portfoliyo_img ,
+};
+
+const dataabout = {
+    title: "About My Self",
+    aboutme: "I'm Kumari Kiran Gupta, currently residing in Kolkata, originally from Dhanbad, Jharkhand. I work as a junior software developer with expertise in JavaScript, including React.js, Axios, React Router, and React Redux Toolkit. I enjoy learning new technologies, browsing the internet, and traveling in my free time.",
+};
+const worktimeline = [{
+    jobtitle: "Junior Software Engineer",
+    where: "Open Solutions For Education India Pvt. Ltd.",
+    date: "May 2023",
+},
+{
+    jobtitle: "Junior Software Engineer Trainee",
+    where: "SysAlgo Technology",
+    date: "Sept 2022",
+},
+
+];
+
+const skills = [{
+    name: "React Js",
+    value: 80,
+},
+{
+    name: "React Native",
+    value: 70,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "Node",
+    value: 60,
+},
+{
+    name: "Jquery",
+    value: 60,
+},
+{
+    name: "Express",
+    value: 60,
+},
+];
+
+const services = [{
+    title: "Frontend Development",
+    description: "Design mockups and wireframes into responsive and functional web pages using HTML, CSS, and JavaScript and frameworks like Reactjs , TypeScript.Write clean, efficient, and maintainable code while actively participating in code reviews and debugging sessions to identify and fix issues.",
+},
+{
+    title: "Mobile Apps",
+    description: "Developing high-quality mobile applications using React Native framework.Building reusable components and libraries for future use.",
+},
+{
+    title: "Backend Development",
+    description: "Mainly focused on Designing and implementing RESTful APIs using Express.js routes,Setting up MongoDB databases and collections",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Its a very basic number Game using html , css , javascript.",
+    link: "https://github.com/kirangupta-coder/Number-Game",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Educational Website using React js , Bootstrap.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Portfoliyo Website using React Js , Bootstrap.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/550/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+},
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "kirangupta4424@gmail.com",
+    YOUR_FONE: "8340575988",
+    description: "I’m Kumari Kiran Gupta Full stack devloper,currently working in Kolkata , India. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/kirangupta-coder",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/kirangupta111/",
+    twitter: "https://twitter.com/KiranGupta111",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
